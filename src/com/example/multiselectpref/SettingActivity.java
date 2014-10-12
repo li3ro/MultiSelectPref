84c31a406258ad55f0d74a9d9cdaa8abb43aace8
vietstone <vietnt88@gmail.com>
2014-10-12 12:16:07 +0700

    remove unused code

--- src/com/example/multiselectpref/SettingActivity.java
+++ src/com/example/multiselectpref/SettingActivity.java
@@ -3,24 +3,16 @@ package com.example.multiselectpref;
 import java.util.HashSet;
 import java.util.Set;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.preference.PreferenceFragment;
 
 
 
-public class SettingActivity extends ActionBarActivity {
+public class SettingActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,26 +22,6 @@ public class SettingActivity extends ActionBarActivity {
                     .replace(android.R.id.content, new SettingsFragment())
                     .commit();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.setting, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
